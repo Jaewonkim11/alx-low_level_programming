abb8f4c5a39807ad82eeae01ac6e3a2c1cfa7be2
Jaewonkim11 <toyinakindelelily@gmail.com>
2022-09-13 15:24:07 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char *sh = "_putchar";
+
+        while (*sh)
+        {
+                putchar(*sh);
+                sh++;
+        }
+        putchar('\n');
+
+        return (0);
+}
