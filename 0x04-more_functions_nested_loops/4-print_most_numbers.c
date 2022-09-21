48fabbd3bfa2284b0325556f441d69a404763f26
Jaewonkim11 <toyinakindelelily@gmail.com>
2022-09-21 01:04:31 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * print_most_numbers -  checks for checks for a digit (0 through 9).
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		if (a != 50)
+		{
+			if (a != 52)
+			{
+			putchar(a);
+			}
+		}
+	}
+	putchar('\n');
+}
